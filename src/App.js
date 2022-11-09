4eb0b2b26dcc1982c34468da4fdc646cd62e5a18
Shaimaa Kamal <Shaimaakamal039@gmail.com>
2022-11-09 17:34:29 +0200

    organize code

--- src/App.js
+++ src/App.js
@@ -8,37 +8,45 @@ import Footer from './components/Footer/Footer';
 class App extends Component 
   {
     componentDidMount(){
+      const self=this;
       const navLinks=document.querySelectorAll('.nav-link');
+        navLinks.forEach((link,index)=>{
+          link.addEventListener('click',function(e){
+            self.activeLink(index);
+          })
+        })
+
+        this.scrollEvent(self,navLinks);  
+    }
+
+    scrollEvent(self,navLinks){
       const Portfolio=document.querySelector('#Portfolio');
       const About=document.querySelector('#About')
       const Contact=document.querySelector('#Contact')
-    navLinks.forEach((link,index)=>{
-      link.addEventListener('click',function(e){
-        navLinks.forEach((inSidelink,Insideindex) => {
-          (index === Insideindex)?inSidelink.classList.add('active'):inSidelink.classList.remove('active');})
-      })
-    })
-    window.addEventListener('scroll',function(e){
-     const scrollVal=Math.ceil(window.scrollY);
-     const nav=document.querySelector('.bg-nav');
-     (scrollVal >= Portfolio.offsetTop)? nav.classList.remove('py-3'): nav.classList.add('py-3');
-    if(scrollVal >= Portfolio.offsetTop && scrollVal < About.offsetTop){
-      navLinks.forEach((link,index) => {
-        (index === 0)?link.classList.add('active'):link.classList.remove('active');})
+      window.addEventListener('scroll',function(){
+        const scrollVal=Math.ceil(window.scrollY);
+        console.log(scrollVal);
+        const nav=document.querySelector('.bg-nav');
+        (scrollVal >= Portfolio.offsetTop)? nav.classList.remove('py-3'): nav.classList.add('py-3');
+       if(scrollVal >= Portfolio.offsetTop && scrollVal < About.offsetTop){
+         self.activeLink(0);
+       }
+       else if(scrollVal >= About.offsetTop && scrollVal < Contact.offsetTop){
+         self.activeLink(1)
+       }
+       else if(scrollVal >= Contact.offsetTop){
+         self.activeLink(2);
+       } else {
+         navLinks.forEach((link,index) => {
+          link.classList.remove('active');})
+       }
+       })
     }
-    else if(scrollVal >= About.offsetTop && scrollVal < Contact.offsetTop){
+
+    activeLink(ind){
+      const navLinks=document.querySelectorAll('.nav-link');
       navLinks.forEach((link,index) => {
-        (index === 1)?link.classList.add('active'):link.classList.remove('active');})
-    }
-    else if(scrollVal >= Contact.offsetTop){
-      navLinks.forEach((link,index) => {
-        (index === 2)?link.classList.add('active'):link.classList.remove('active');})
-    } else {
-      navLinks.forEach((link,index) => {
-       link.classList.remove('active');})
-    }
-    })
-  
+        (index === ind)?link.classList.add('active'):link.classList.remove('active');})
     }
     render(){
       return (
